6f8db5373dab392a42b32eb95feadaae9bed3c77
VinuBalagopalAP <vinubalagopalap@gmail.com>
2022-06-09 13:17:41 +0530

    app initialized 🤘🏻

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,13 @@
-import 'package:effigy/firebase_options.dart';
-import 'package:effigy/screens/home/home_page.dart';
-import 'package:effigy/screens/login/auth_page.dart';
-import 'package:firebase_auth/firebase_auth.dart';
+import 'package:effigy/screens/auth/auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'firebase_options.dart';
 
 void main() async {
-  /// [ ensureInitialized ] is a method that ensures that the Firebase is initialized.
+  /// [ ensureInitialized ] to ensure that the Firebase app is initialized.
   WidgetsFlutterBinding.ensureInitialized();
 
-  /// [ Firebase.initializeApp ] is a method to initialized the Firebase.
+  /// [ initializeApp ] to wait for the Firebase app to be initialized.
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
@@ -17,53 +15,14 @@ void main() async {
   runApp(const MyApp());
 }
 
-final navigatorKey = GlobalKey<NavigatorState>();
-
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      navigatorKey: navigatorKey,
-      title: 'Effigy App',
-      debugShowCheckedModeBanner: false,
-      home: StreamBuilder<User?>(
-        /// [ FirebaseAuth.instance.currentUser ] is null if the user is not logged in.
-        stream: FirebaseAuth.instance.authStateChanges(),
-
-        builder: (context, snapshot) {
-          switch (snapshot.connectionState) {
-
-            /// [ Waiting ] for [ FirebaseAuth.instance.currentUser ] to be set.
-            case ConnectionState.waiting:
-
-              /// [ Circular progress indicator ] is shown while waiting for [ FirebaseAuth.instance.currentUser ] to be set.
-              return const Center(child: CircularProgressIndicator());
-
-            /// [ ConnectionState.done ] is reached when [ FirebaseAuth.instance.currentUser ] is set.
-            case ConnectionState.done:
-
-              /// [ snapshot.hasData ] to show [ HomePage ] if the user is logged in.
-              if (snapshot.hasData) {
-                return const HomePage();
-              }
-
-              /// [ AuthPage ] is shown if the user is not logged in.
-              return const AuthPage();
-
-            default:
-
-              /// [ snapshot.hasError ] to show Error message if any error occurs
-              if (snapshot.hasError) {
-                return const Center(child: Text('An error occurred'));
-              }
-
-              /// [ AuthPage ] is shown if the user is not logged in.
-              return const AuthPage();
-          }
-        },
-      ),
+    return const MaterialApp(
+      title: 'Material App',
+      home: AuthPage(),
     );
   }
 }
